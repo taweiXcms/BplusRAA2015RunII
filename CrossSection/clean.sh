8a7bcf179e7ea1af1163412d75ef2fe110c1a8c1
ginnocen <ginnocen@cern.ch>
2016-06-09 18:50:13 +0200

    adding clean macro

---
+++ CrossSection/clean.sh
@@ -0,0 +1,21 @@
+rm *.so
+rm *.d
+rm *.eps
+rm *.pdf
+rm *.root
+rm *.pcm
+rm *.exe
+rm an.log
+
+rm ROOTfiles/*.*
+rm ROOTfilesCent10/*.*
+
+rm Fits/*.*
+rm FitsVariables/*.*
+rm plot*/*.*
+git checkout ROOTfiles
+git checkout plotCrossSection 
+git checkout plotFits 
+git checkout plotOthers 
+git checkout plotEff
+
