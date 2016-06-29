42f60361a2d69e0968f10c40253cf90b98f44e82
ginnocen <ginnocen@cern.ch>
2016-06-29 18:25:27 +0200

    fix

--- CrossSection/doAnalysisVariable.sh
+++ CrossSection/doAnalysisVariable.sh
@@ -15,7 +15,6 @@ cp config/parametersVariablesDecay.h parameters.h
 VARIABLE="BsvpvDistance/BsvpvDisErr";
 VARIABLEPLOT="d_{xy}/#sigma(d_{xy})"
 VARIABLENAME="decay"
-LABELPPPromptMC="MCPromptDecay"
 OUTPUTFILEPPVariablePromptMC="MCCutVariablePromptDecay.root"
 OUTPUTFILEPPVariableData="DataCutVariableDecay.root"
 LABELPPData="DecayData"
@@ -24,7 +23,35 @@ LIMITLOW=3
 LIMITHIGH=12
 fi
 
+if [ $VariableOption -eq 1 ]; then      
 
+cp config/parametersVariablesCosTheta.h parameters.h 
+
+VARIABLE="cos(Bdtheta) "
+VARIABLEPLOT="cosine (#theta)"
+VARIABLENAME="cosine"
+OUTPUTFILEPPVariablePromptMC="MCCutVariablePromptCosine.root"
+OUTPUTFILEPPVariableData="DataCutVariableCosine.root"
+LABELPPData="CosineData"
+LABELPPMC="CosineMC"
+LIMITLOW=0.99
+LIMITHIGH=1
+fi
+
+if [ $VariableOption -eq 2 ]; then      
+
+cp config/parametersVariablesChi2.h parameters.h 
+
+VARIABLE="Bchi2cl"
+VARIABLEPLOT="chi2"
+VARIABLENAME="chi2"
+OUTPUTFILEPPVariablePromptMC="MCCutVariablePromptChi2.root"
+OUTPUTFILEPPVariableData="DataCutVariableChi2.root"
+LABELPPData="Chi2Data"
+LABELPPMC="Chi2MC"
+LIMITLOW=0.
+LIMITHIGH=1
+fi
 
 LUMIPP=1
 ISMCPP=0
@@ -38,8 +65,8 @@ TRGPPData="HLT_HIL1DoubleMu0_v1"
 
 
 g++ fitBvariable.C $(root-config --cflags --libs) -g -o fitBvariable.exe 
-./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTDATAPP"  "$INPUTMCPP"  "$TRGPPData" "$CUTPP"   "$SELGENPP"   "$ISMCPP"          1   "$ISDOWEIGHTPP"   "$LABELPPData"  "$OUTPUTFILEPPVariableData" "$OUTPUTFILEPP_NP"
-./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTMCPP"      "$INPUTMCPP"  "$TRGPPMC"   "$CUTPP"   "$SELGENPP"   "$ISMCPPData"   1   "$ISDOWEIGHTPP"   "$LABELPPMC"  "$OUTPUTFILEPPVariablePromptMC" "$OUTPUTFILEPP_NP"
+#./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTDATAPP"  "$INPUTMCPP"  "$TRGPPData" "$CUTPP"   "$SELGENPP"   "$ISMCPP"          1   "$ISDOWEIGHTPP"   "$LABELPPData"  "$OUTPUTFILEPPVariableData" "$OUTPUTFILEPP_NP"
+#./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTMCPP"      "$INPUTMCPP"  "$TRGPPMC"   "$CUTPP"   "$SELGENPP"   "$ISMCPPData"   1   "$ISDOWEIGHTPP"   "$LABELPPMC"  "$OUTPUTFILEPPVariablePromptMC" "$OUTPUTFILEPP_NP"
 
 g++ compareMCdataVariables.C $(root-config --cflags --libs) -g -o compareMCdataVariables.exe 
 ./compareMCdataVariables.exe "$OUTPUTFILEPPVariablePromptMC" "$OUTPUTFILEPPVariableData" "$VARIABLEPLOT" "$VARIABLENAME" "$LIMITLOW" "$LIMITHIGH"
