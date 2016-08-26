494a0c8ef1bed9a9c01dbca95bd2d3953bfc5bfc
Jing Wang <boundino@gmail.com>
2016-08-26 10:23:52 +0200

    update Data MC comparison

--- CrossSection/compareMCdataVariables.C
+++ CrossSection/compareMCdataVariables.C
@@ -1,39 +1,46 @@
-#include <TTree.h>
-#include <TH1D.h>
-#include <TFile.h>
-#include <TString.h>
-#include <TCut.h>
-#include <TCanvas.h>
-#include <TF1.h>
-#include <TLegend.h>
-#include <TLegendEntry.h>
+using namespace std;
 #include "uti.h"
 #include "parameters.h"
 
-void compareMCdataVariables(TString promptfile="MCCutVariablePromptPt.root",TString datafile="DataCutVariablePt.root",TString cutvariable="Dpt",TString label="Dpt", double limitlow=0, double limitup=100){
-
+int compareMCdataVariables(TString promptfile, TString datafile,
+                           TString variableplot, TString variablename,
+                           Int_t usePbPb)
+{
+  gStyle->SetTextSize(0.05);
+  gStyle->SetTextFont(42);
+  gStyle->SetPadRightMargin(0.043);
+  gStyle->SetPadLeftMargin(0.18);
+  gStyle->SetPadTopMargin(0.1);
+  gStyle->SetPadBottomMargin(0.145);
+  gStyle->SetTitleX(.0f);
   gStyle->SetOptTitle(0);
   gStyle->SetOptStat(0);
   gStyle->SetEndErrorSize(0);
   gStyle->SetMarkerStyle(20);
 
+  if(!(usePbPb==1||usePbPb==0))
+    {
+      cout<<" ERROR: invalide usePbPb"<<endl;
+      return 1;
+    }
+  Bool_t isPbPb = (Bool_t)(usePbPb);
+  TString collisionsystem = (isPbPb)?"PbPb":"pp";
 
-  TFile* infMCprompt = new TFile(promptfile.Data());
+  TFile* infMCprompt = new TFile(Form("%s_%s_%s.root",promptfile.Data(),collisionsystem.Data(),variablename.Data()));
   TH1F* hPtprompt = (TH1F*)infMCprompt->Get("hPt");
-  TFile* infData = new TFile(datafile.Data());
+  TFile* infData = new TFile(Form("%s_%s_%s.root",datafile.Data(),collisionsystem.Data(),variablename.Data()));
   TH1F* hPtData= (TH1F*)infData->Get("hPt");
 
   hPtprompt->Scale(1./hPtprompt->Integral(1,nBins));
-  hPtData->Scale(1./hPtData->Integral(1,nBins));    
+  hPtData->Scale(1./hPtData->Integral(1,nBins));
 
-
-  TH2F* hempty=new TH2F("hempty","",100,limitlow,limitup,10,0.01,5);  
+  TH2F* hempty = new TH2F("hempty","",100,limitlow,limithigh,10,limitmin,limitmax);
   hempty->GetXaxis()->CenterTitle();
   hempty->GetYaxis()->CenterTitle();
-  hempty->GetYaxis()->SetTitle("Yield / GeV/c^{2}");
-  hempty->GetXaxis()->SetTitle(cutvariable.Data());
+  hempty->GetYaxis()->SetTitle("Probability");
+  hempty->GetXaxis()->SetTitle(variableplot.Data());
   hempty->GetXaxis()->SetTitleOffset(0.9);
-  hempty->GetYaxis()->SetTitleOffset(1.1);
+  hempty->GetYaxis()->SetTitleOffset(1.3);
   hempty->GetXaxis()->SetTitleSize(0.045);
   hempty->GetYaxis()->SetTitleSize(0.045);
   hempty->GetXaxis()->SetTitleFont(42);
@@ -43,47 +50,69 @@ void compareMCdataVariables(TString promptfile="MCCutVariablePromptPt.root",TStr
   hempty->GetXaxis()->SetLabelSize(0.035);
   hempty->GetYaxis()->SetLabelSize(0.035);  
 
-  TCanvas *canvas = new TCanvas("canvas","",600,600);
+  hPtprompt->SetMarkerStyle(20);
+  hPtprompt->SetMarkerColor(kRed);
+  hPtprompt->SetMarkerSize(1.1);
+  hPtprompt->SetLineColor(kRed);
+  hPtprompt->SetLineWidth(1);
+  hPtData->SetMarkerStyle(20);
+  hPtData->SetMarkerColor(kBlue+1);
+  hPtData->SetMarkerSize(1.1);
+  hPtData->SetLineColor(kBlue+1);
+  hPtData->SetLineWidth(1);
+
+  TCanvas* canvas = new TCanvas("canvas","",600,600);
   canvas->cd(1);
   gPad->SetLogy();
   hempty->Draw();
-  hPtprompt->SetLineColor(2);
-  hPtprompt->SetLineWidth(2);
-  hPtprompt->Draw("same");
-  hPtData->SetLineColor(1);
-  hPtData->SetLineWidth(2);
   hPtData->Draw("same");
-  TLegend *legendOrig=new TLegend(0.2958166,0.6558707,0.5949297,0.8299148,"");
-  legendOrig->SetBorderSize(0);
-  legendOrig->SetLineColor(0);
-  legendOrig->SetFillColor(0);
-  legendOrig->SetFillStyle(1001);
-  legendOrig->SetTextFont(42);
-  legendOrig->SetTextSize(0.045);
-  TLegendEntry *ent_DataOrig=legendOrig->AddEntry(hPtData,"Data (norm=1)","pf");
-  ent_DataOrig->SetTextFont(42);
-  ent_DataOrig->SetLineColor(1);
-  TLegendEntry *ent_MCP=legendOrig->AddEntry(hPtprompt,"MC prompt (norm=1)","pf");
-  ent_MCP->SetTextFont(42);
-  ent_MCP->SetLineColor(2);
-  legendOrig->Draw("same");
-  canvas->SaveAs(Form("canvasMCDataComp_%s.pdf",label.Data()));
+  hPtprompt->Draw("same");
 
+  TLegend* legPtY = new TLegend(0.08,0.75,0.60,0.86);
+  legPtY->SetTextSize(0.045);
+  legPtY->SetBorderSize(0);
+  legPtY->SetFillStyle(0);
+  legPtY->AddEntry((TObject*)0,"|y| < 2.4",NULL);
+  legPtY->AddEntry((TObject*)0,"7 < p_{T} < 50 GeV/c",NULL);
+  legPtY->Draw("same");
+
+  TLegend* leg = new TLegend(0.62,0.75,0.95,0.86);
+  leg->SetTextSize(0.045);
+  leg->SetBorderSize(0);
+  leg->SetFillStyle(0);
+  leg->AddEntry(hPtprompt,"Monte-Carlo","pl");
+  leg->AddEntry(hPtData,"Data","pl");
+  leg->Draw("same");
+
+  TLatex* texCms = new TLatex(0.18,0.93, "#scale[1.25]{CMS} Preliminary");
+  texCms->SetNDC();
+  texCms->SetTextAlign(12);
+  texCms->SetTextSize(0.04);
+  texCms->SetTextFont(42);
+  texCms->Draw();
+
+  TLatex* texCol = new TLatex(0.96,0.93, Form("%s #sqrt{s_{NN}} = 5.02 TeV",collisionsystem.Data()));
+  texCol->SetNDC();
+  texCol->SetTextAlign(32);
+  texCol->SetTextSize(0.04);
+  texCol->SetTextFont(42);
+  texCol->Draw();
+
+  canvas->SaveAs(Form("plotVariablesResults/Comparison_%s_%s.pdf",collisionsystem.Data(),variablename.Data()));
+  return 0;
 }
 
 
 int main(int argc, char *argv[])
 {
-  if((argc != 7))
-  {
-    std::cout << "Wrong number of inputs" << std::endl;
-    return 1;
-  }
-
-  if(argc ==7)
-    compareMCdataVariables(argv[1],argv[2],argv[3],argv[4], atof(argv[5]),atof(argv[6]));
-  return 0;
+  if(argc==6)
+    {
+      compareMCdataVariables(argv[1], argv[2], argv[3], argv[4], atoi(argv[5]));
+      return 0;
+    }
+  else
+    {
+      cout<<"  ERROR: Wrong number of inputs"<<endl;
+      return 1;
+    }
 }
-
-
-
--- CrossSection/doAnalysisVariable.sh
+++ CrossSection/doAnalysisVariable.sh
@@ -1,106 +1,86 @@
 #!/bin/bash
 #source clean.sh
 
-VariableOption=$1
+VariableOption=6
 
-INPUTMCPP="/data/HeavyFlavourRun2/MC2015/Bntuple/pp/Bntuple20160629_Bpt7svpv5p5Bpt10svpv3p5_pp_Pythia8_BuToJpsiK_Bpt5p0_Pthat5_BDT.root" 
-INPUTDATAPP="/data/HeavyFlavourRun2/Data2015/Bntuple/Bntuple20160629_Bpt7svpv5p5Bpt10svpv3p5_crab_BfinderData_pp_20160606_bPt0jpsiPt0tkPt0p5_Bp_BDT.root"
+ISPbPb=0
+ISDOWEIGHTPP=1
 
-OUTPUTFILEPP_NP="ROOTfiles/NPFitPP.root"
+INPUTMCPP="/data/HeavyFlavourRun2/MC2015/Bntuple/pp/Bntuple20160816_Bpt7svpv5p5Bpt10svpv3p5_BfinderMC_pp_Pythia8_BuToJpsiK_TuneCUEP8M1_20160816_bPt0jpsiPt0tkPt0p5_Bp_pthatweight_JingBDT.root" 
+INPUTDATAPP="/data/HeavyFlavourRun2/Data2015/Bntuple/Bntuple20160816_Bpt7svpv5p5Bpt10svpv3p5_BfinderData_pp_20160816_bPt0jpsiPt0tkPt0p5_Bp_JingBDT.root"
+
+##
+
+OUTPUTFILEMC="outfilesVariable/Variable_MC"
+OUTPUTFILEData="outfilesVariable/Variable_Data"
+
 NPFIT_PP="3.12764e1*Gaus(x,5.33166,3.64663e-2)*(x<5.33166)+(x>=5.33166)*3.12764e1*Gaus(x,5.33166,1.5204e-1)+7.18255e1*TMath::Erf(-(x-5.14397)/6.43194e-2) + 7.18255e1"
 
-if [ $VariableOption -eq 0 ]; then      
+##
 
+if [ $VariableOption -eq 0 ]; then
 cp config/parametersVariablesDecay.h parameters.h 
-
-VARIABLE="BsvpvDistance/BsvpvDisErr";
-VARIABLEPLOT="d_{xy}/#sigma(d_{xy})"
+VARIABLE="(BsvpvDistance/BsvpvDisErr)";
+VARIABLEPLOT="d_{0}/#sigma(d_{0})"
 VARIABLENAME="decay"
-OUTPUTFILEPPVariablePromptMC="MCCutVariablePromptDecay.root"
-OUTPUTFILEPPVariableData="DataCutVariableDecay.root"
-LABELPPData="DecayData"
-LABELPPMC="DecayMC"
-LIMITLOW=3
-LIMITHIGH=12
 fi
 
-if [ $VariableOption -eq 1 ]; then      
-
+if [ $VariableOption -eq 1 ]; then
 cp config/parametersVariablesCosTheta.h parameters.h 
-
 VARIABLE="cos(Bdtheta) "
-VARIABLEPLOT="cosine (#theta)"
+VARIABLEPLOT="cos#theta"
 VARIABLENAME="cosine"
-OUTPUTFILEPPVariablePromptMC="MCCutVariablePromptCosine.root"
-OUTPUTFILEPPVariableData="DataCutVariableCosine.root"
-LABELPPData="CosineData"
-LABELPPMC="CosineMC"
-LIMITLOW=0.99
-LIMITHIGH=1
 fi
 
-if [ $VariableOption -eq 2 ]; then      
-
+if [ $VariableOption -eq 2 ]; then
 cp config/parametersVariablesChi2.h parameters.h 
-
 VARIABLE="Bchi2cl"
-VARIABLEPLOT="chi2"
+VARIABLEPLOT="#chi^{2} prob"
 VARIABLENAME="chi2"
-OUTPUTFILEPPVariablePromptMC="MCCutVariablePromptChi2.root"
-OUTPUTFILEPPVariableData="DataCutVariableChi2.root"
-LABELPPData="Chi2Data"
-LABELPPMC="Chi2MC"
-LIMITLOW=0.
-LIMITHIGH=1
 fi
 
-if [ $VariableOption -eq 3 ]; then      
+if [ $VariableOption -eq 3 ]; then
+cp config/parametersVariablesRapidity.h parameters.h 
+VARIABLE="By"
+VARIABLEPLOT="y^{B}"
+VARIABLENAME="rapidity"
+fi
 
+if [ $VariableOption -eq 4 ]; then
+cp config/parametersVariablesTracketa.h parameters.h
+VARIABLE="Btrk1Eta"
+VARIABLEPLOT="#eta^{track}"
+VARIABLENAME="trketa"
+fi
+
+if [ $VariableOption -eq 5 ]; then
+cp config/parametersVariablesTrackpt.h parameters.h
+VARIABLE="Btrk1Pt"
+VARIABLEPLOT="p_{T}^{track} (GeV/c)"
+VARIABLENAME="trkpt"
+fi
+
+if [ $VariableOption -eq 6 ]; then
 cp config/parametersVariablesBDT.h parameters.h 
-
 VARIABLE="BDT"
 VARIABLEPLOT="BDT"
 VARIABLENAME="BDT"
-OUTPUTFILEPPVariablePromptMC="MCCutVariableBDT.root"
-OUTPUTFILEPPVariableData="DataCutVariableBDT.root"
-LABELPPData="BDTData"
-LABELPPMC="BDTMC"
-LIMITLOW=-1
-LIMITHIGH=2
-fi
-
-if [ $VariableOption -eq 4 ]; then      
-
-cp config/parametersVariablesrapidity.h parameters.h 
-
-VARIABLE="By"
-VARIABLEPLOT="rapidity"
-VARIABLENAME="rapidity"
-OUTPUTFILEPPVariablePromptMC="MCCutVariablerapidity.root"
-OUTPUTFILEPPVariableData="DataCutVariablerapidity.root"
-LABELPPData="rapidityData"
-LABELPPMC="rapidityTMC"
-LIMITLOW=-1.5
-LIMITHIGH=1.5
 fi
 
 
-LUMIPP=1
-ISMCPP=0
-ISMCPPData=0
-ISDOWEIGHTPP=0
-SELGENPP="TMath::Abs(Gy)<2.4&&abs(GpdgId)==521&&GisSignal==1"
-SELGENPPACCPP="TMath::Abs(Gy)<2.4&&abs(GpdgId)==521&&GisSignal==1"
-CUTPP="abs(PVz)<15&&pBeamScrapingFilter&&pPAprimaryVertexFilter&&TMath::Abs(By)<2.4&&TMath::Abs(Bmumumass-3.096916)<0.15&&Bmass>5&&Bmass<6&& ((abs(Bmu1eta)<1.2 && Bmu1pt>3.5) || (abs(Bmu1eta)>1.2 && abs(Bmu1eta)<2.1 && Bmu1pt>(5.77-1.8*abs(Bmu1eta))) || (abs(Bmu1eta)>2.1 && abs(Bmu1eta)<2.4 && Bmu1pt>1.8)) && ((abs(Bmu2eta)<1.2 && Bmu2pt>3.5) || (abs(Bmu2eta)>1.2 && abs(Bmu2eta)<2.1 && Bmu2pt>(5.77-1.8*abs(Bmu2eta))) || (abs(Bmu2eta)>2.1 && abs(Bmu2eta)<2.4 && Bmu2pt>1.8)) && Bmu1TMOneStationTight && Bmu2TMOneStationTight && Bmu1InPixelLayer > 0 && (Bmu1InPixelLayer+Bmu1InStripLayer) > 5 && Bmu2InPixelLayer > 0 && (Bmu2InPixelLayer+Bmu2InStripLayer) > 5 && Bmu1dxyPV< 0.3 && Bmu2dxyPV< 0.3 && Bmu1dzPV<20 && Bmu2dzPV<20 && Bmu1isGlobalMuon && Bmu2isGlobalMuon && Btrk1Pt>1. && abs(Beta) < 2.4 && Bchi2cl > 0.005 && Btrk1highPurity && abs(Btrk1Eta)<2.4 && cos(Bdtheta) > 0.2 && (BsvpvDistance/BsvpvDisErr)>4."
 TRGPPMC="(1)"
 TRGPPData="HLT_HIL1DoubleMu0_v1"
+CUTPP="abs(PVz)<15&&pBeamScrapingFilter&&pPAprimaryVertexFilter&&TMath::Abs(By)<2.4&&TMath::Abs(Bmumumass-3.096916)<0.15&&Bmass>5&&Bmass<6&& ((abs(Bmu1eta)<1.2 && Bmu1pt>3.5) || (abs(Bmu1eta)>1.2 && abs(Bmu1eta)<2.1 && Bmu1pt>(5.77-1.8*abs(Bmu1eta))) || (abs(Bmu1eta)>2.1 && abs(Bmu1eta)<2.4 && Bmu1pt>1.8)) && ((abs(Bmu2eta)<1.2 && Bmu2pt>3.5) || (abs(Bmu2eta)>1.2 && abs(Bmu2eta)<2.1 && Bmu2pt>(5.77-1.8*abs(Bmu2eta))) || (abs(Bmu2eta)>2.1 && abs(Bmu2eta)<2.4 && Bmu2pt>1.8)) && Bmu1TMOneStationTight && Bmu2TMOneStationTight && Bmu1InPixelLayer > 0 && (Bmu1InPixelLayer+Bmu1InStripLayer) > 5 && Bmu2InPixelLayer > 0 && (Bmu2InPixelLayer+Bmu2InStripLayer) > 5 && Bmu1dxyPV< 0.3 && Bmu2dxyPV< 0.3 && Bmu1dzPV<20 && Bmu2dzPV<20 && Bmu1isGlobalMuon && Bmu2isGlobalMuon && Bmu1TrgMatchFilterE>0 && Bmu2TrgMatchFilterE>0 && Btrk1highPurity && abs(Btrk1Eta)<2.4 && Btrk1Pt>0.5 && Bchi2cl>0.005 && (BsvpvDistance/BsvpvDisErr)>4."
+
 
 
 g++ fitBvariable.C $(root-config --cflags --libs) -g -o fitBvariable.exe 
-#./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTDATAPP"  "$INPUTMCPP"  "$TRGPPData" "$CUTPP"   "$SELGENPP"   "$ISMCPP"          1   "$ISDOWEIGHTPP"   "$LABELPPData"  "$OUTPUTFILEPPVariableData" "$OUTPUTFILEPP_NP"
-#./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTMCPP"      "$INPUTMCPP"  "$TRGPPMC"   "$CUTPP"   "$SELGENPP"   "$ISMCPPData"   1   "$ISDOWEIGHTPP"   "$LABELPPMC"  "$OUTPUTFILEPPVariablePromptMC" "$OUTPUTFILEPP_NP"
-./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTDATAPP"  "$INPUTMCPP"  "$TRGPPData" "$CUTPP"   "$SELGENPP"   "$ISMCPP"          1   "$ISDOWEIGHTPP"   "$LABELPPData"  "$OUTPUTFILEPPVariableData" "$NPFIT_PP"
-./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" 0 "$INPUTMCPP"      "$INPUTMCPP"  "$TRGPPMC"   "$CUTPP"   "$SELGENPP"   "$ISMCPPData"   1   "$ISDOWEIGHTPP"   "$LABELPPMC"  "$OUTPUTFILEPPVariablePromptMC" "$NPFIT_PP"
+./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" "$VARIABLENAME" "$ISPbPb" 0 "$INPUTDATAPP" "$INPUTMCPP" "$TRGPPData" "$CUTPP" "$ISDOWEIGHTPP" "$OUTPUTFILEData" "$NPFIT_PP"
+./fitBvariable.exe "$VARIABLE" "$VARIABLEPLOT" "$VARIABLENAME" "$ISPbPb" 1 "$INPUTMCPP" "$INPUTMCPP" "$TRGPPMC" "$CUTPP" "$ISDOWEIGHTPP" "$OUTPUTFILEMC" "$NPFIT_PP"
 
 g++ compareMCdataVariables.C $(root-config --cflags --libs) -g -o compareMCdataVariables.exe 
-./compareMCdataVariables.exe "$OUTPUTFILEPPVariablePromptMC" "$OUTPUTFILEPPVariableData" "$VARIABLEPLOT" "$VARIABLENAME" "$LIMITLOW" "$LIMITHIGH"
+./compareMCdataVariables.exe "$OUTPUTFILEMC" "$OUTPUTFILEData" "$VARIABLEPLOT" "$VARIABLENAME" "$ISPbPb"
+
+rm fitBvariable.exe
+rm compareMCdataVariables.exe
+rm parameters.h
